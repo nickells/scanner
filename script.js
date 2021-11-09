f8f507899666699eccffab802081fc792c4cec2d
Glitch (glitch-hello-website) <none>
2021-11-09 23:39:19 +0000

    ⛳️😚 Checkpoint ./script.js:1112019/2701 ./index.html:1112019/326 ./style.css:1112019/140

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const canvas = document.getElementById('canvas')
+const canvas1 = document.getElementById('canvas1')
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas1.width = window.innerWidth
+canvas1.height = window.innerHeight
 
-const context = canvas.getContext('2d')
+const context1 = canvas1.getContext('2d')
 
 const image = new Image()
 image.src = 'https://cdn.glitch.me/8f207867-584a-4e33-ba8a-e5f07660b828%2F1.png?v=1636498924289'
@@ -14,7 +14,7 @@ image.addEventListener('load', evt => {
   state.height = image.height / 2
 })
 
-canvas.addEventListener('mousemove', evt => {
+canvas1.addEventListener('mousemove', evt => {
   const x = evt.clientX
   const y = evt.clientY
   
@@ -27,25 +27,37 @@ canvas.addEventListener('mousemove', evt => {
 })
 
 
-canvas.addEventListener('mousedown', evt => {
+canvas1.addEventListener('mousedown', evt => {
   state.scanning = true
   state.scanX = 0
 })
 
 const render = () => {
-  context.clearRect(0, 0, canvas.width, canvas.height)
-  context.drawImage(image, state.imageX, state.imageY, state.width, state.height)
+  context1.clearRect(state.scanX, 0, canvas1.width, canvas1.height)
+  context1.drawImage(image, state.imageX, state.imageY, state.width, state.height)
+
   
   if (state.scanning) {
     state.scanX += 2
-    context.strokeStyle = 'black'
-    context.strokeWidth = 1
-    context.beginPath()
-    context.moveTo(state.scanX, 0)
-    context.lineTo(state.scanX, canvas.height)
-    context.stroke()  
-    context.drawImage(canvas, 0, 0, state.scanX, canvas.height)
+    context1.strokeStyle = 'black'
+    context1.strokeWidth = 1
+    context1.beginPath()
+    context1.moveTo(state.scanX, 0)
+    context1.lineTo(state.scanX, canvas1.height)
+    context1.stroke()  
+    context1.drawImage(
+      canvas1, 
+      0, // source X start
+      0,  // source Y start
+      state.scanX, // source width
+      canvas1.height, // source height
+      0, // destination X start
+      0, // destination Y start
+      state.scanX, // destination width
+      canvas1.height) // destination height
   }
+    
+    
   
   requestAnimationFrame(render)
 }
