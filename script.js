fb44f51e8f1e637420abc2a6476271e79a37e8c9
Glitch (glitch-hello-website) <none>
2021-11-09 23:15:25 +0000

    🐀🦁 Checkpoint ./script.js:1112019/1966

--- script.js
+++ script.js
@@ -6,26 +6,33 @@ canvas.height = window.innerHeight
 const context = canvas.getContext('2d')
 
 const image = new Image()
-image.src = 'https://via.placeholder.com/512x512'
+image.src = 'https://cdn.glitch.me/8f207867-584a-4e33-ba8a-e5f07660b828%2F1.png?v=1636498924289'
 
-image.addEventListener('load', event => {
-  console.log(event)
-  context.drawImage(image, 0, 0)
+let state = {}
+image.addEventListener('load', evt => {
+  state.width = image.width / 2,
+  state.height = image.height / 2
 })
-
-const state = {}
 
 canvas.addEventListener('mousemove', evt => {
   const x = evt.clientX
   const y = evt.clientY
   
-  const imageX = x - (image.width / 2)
-  const imageY = y - (image.height / 2 )
-  context.drawImage(image, imageX, imageY)
+  
+  const imageX = x - (state.width / 2)
+  const imageY = y - (state.height / 2 )
+  state.imageX = imageX
+  state.imageY = imageY
+  
+})
+
+canvas.addEventListener('mousedown', evt => {
+  
 })
 
 const render = () => {
-  
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  context.drawImage(image, state.imageX, state.imageY, state.width, state.height)
   requestAnimationFrame(render)
 }
 
