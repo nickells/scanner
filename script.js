4d8c631b6c0943bab1fae7c3c43ef8876d91caf7
Glitch (glitch-hello-website) <none>
2021-11-09 23:01:31 +0000

    👧🌠 Checkpoint ./index.html:1112019/9637 ./style.css:1112019/12474 ./script.js:1112019/2096 ./LICENSE:1112019/2414

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+const canvas = document.getElementById('canvas')
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+const context = canvas.getContext('2d')
+
+const image = new Image()
+image.src = 'https://via.placeholder.com/512x512'
+
+image.addEventListener('load', event => {
+  console.log(event)
+  context.drawImage(image, 0, 0)
+})
+
+const state = {}
+
+canvas.addEventListener('mousemove', evt => {
+  const x = evt.clientX
+  const y = evt.clientY
+  
+  const imageX = x - (image.width / 2)
+  const imageY = y - (image.height / 2 )
+  context.drawImage(image, imageX, imageY)
+})
+
+const render = () => {
+  
+  requestAnimationFrame(render)
 }
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+render()
