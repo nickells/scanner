2932b14e54ed126013b282175bb020589ffb21e6
Glitch (glitch-hello-website) <none>
2021-11-12 20:38:32 +0000

    ⛳️🔖 Checkpoint ./script.js:1112019/5498 ./index.html:1112019/104

--- script.js
+++ script.js
@@ -1,113 +1,107 @@
-const canvas1 = document.getElementById('canvas')
+const canvas1 = document.getElementById("canvas");
 
-canvas1.width = window.innerWidth
-canvas1.height = window.innerHeight
+canvas1.width = window.innerWidth;
+canvas1.height = window.innerHeight;
 
-const context1 = canvas1.getContext('2d')
+const context1 = canvas1.getContext("2d");
 
-const canvas2 = document.getElementById('canvas-top')
+const canvas2 = document.getElementById("canvas-top");
 
-canvas2.width = window.innerWidth
-canvas2.height = window.innerHeight
+canvas2.width = window.innerWidth;
+canvas2.height = window.innerHeight;
 
-const context2 = canvas2.getContext('2d')
+const context2 = canvas2.getContext("2d");
 
+const image = new Image();
+image.src =
+  "https://cdn.glitch.me/1be3bc59-b422-4635-9476-f11ca80a5d2a%2Fnick.jpg?v=1636743423676";
 
-const image = new Image()
-image.src = 'https://cdn.glitch.me/1be3bc59-b422-4635-9476-f11ca80a5d2a%2Fnick.jpg?v=1636743423676'
+const scanner = document.getElementById("scanner");
 
-const scanner = document.getElementById('scanner')
+const tween = window.TWEEN
+
 
 let state = {
   scanX: 0,
   done: false,
-  scanning: false,
-}
-image.addEventListener('load', evt => {
-  state.width = image.width / 2,
-  state.height = image.height / 2
-})
+  scanning: false
+};
+image.addEventListener("load", evt => {
+  (state.width = image.width / 2), (state.height = image.height / 2);
+});
 
-canvas1.addEventListener('mousemove', evt => {
-  const x = evt.clientX
-  const y = evt.clientY
-  
-  
-  const imageX = x - (state.width / 2)
-  const imageY = y - (state.height / 2 )
-  state.imageX = imageX
-  state.imageY = imageY
-  
-})
+canvas1.addEventListener("mousemove", evt => {
+  const x = evt.clientX;
+  const y = evt.clientY;
 
+  const imageX = x - state.width / 2;
+  const imageY = y - state.height / 2;
+  state.imageX = imageX;
+  state.imageY = imageY;
+});
 
-document.addEventListener('keydown', evt => {
-  if (evt.code === 'Space') {
-    
+document.addEventListener("keydown", evt => {
+  if (evt.code === "Space") {
     if (state.scanning === false) {
-      state.scanning = true
-      
+      state.scanning = true;
+
       if (state.scanX >= canvas1.width) {
         state.scanX = 0;
-        context1.clearRect(0, 0, canvas1.width, canvas1.height)
-        context2.clearRect(0, 0, canvas1.width, canvas1.height)
-        canvas2.style.pointerEvents = 'none'
-        canvas1.style.pointerEvents = 'all'
+        context1.clearRect(0, 0, canvas1.width, canvas1.height);
+        context2.clearRect(0, 0, canvas1.width, canvas1.height);
+        canvas2.style.pointerEvents = "none";
+        canvas1.style.pointerEvents = "all";
       }
-    }
-    else {
+    } else {
       state.scanning = false;
     }
-    
   }
-})
+});
 
-const scan_width = 1
+const scan_width = 1;
 
-
-const render = () => {
-  
+const render = (time) => {
   // draws on first image
-  context1.clearRect(0, 0, canvas1.width, canvas1.height)
+  context1.clearRect(0, 0, canvas1.width, canvas1.height);
   context1.drawImage(
-    image, 
+    image,
     state.imageX,
     state.imageY,
     state.width,
-    state.height,
-  )
+    state.height
+  );
 
   // context2.clearRect(state.scanX, 0, canvas2.width, canvas2.height)
-  
+
   if (state.scanning) {
-    state.scanX += scan_width
-    
-    scanner.style.left = state.scanX + 'px'
-    
+    state.scanX += scan_width;
+
+    scanner.style.left = state.scanX + "px";
+
     // keep contents of fi rst image on second image
     context2.drawImage(
-      canvas1, 
+      canvas1,
       state.scanX, // source X start
-      0,  // source Y start
+      0, // source Y start
       scan_width, // source width
       canvas1.height, // source height
       state.scanX, // destination X start
       0, // destination Y start
       scan_width, // destination width
-      canvas1.height) // destination height
+      canvas1.height
+    ); // destination height
   }
-  
+
   if (state.scanX >= canvas1.width) {
     state.scanning = false;
     state.done = true;
-    canvas1.style.pointerEvents = 'none'
-    canvas2.style.pointerEvents = 'all'
-    context1.clearRect(0, 0, canvas1.width, canvas1.height)
+    canvas1.style.pointerEvents = "none";
+    canvas2.style.pointerEvents = "all";
+    context1.clearRect(0, 0, canvas1.width, canvas1.height);
   }
-    
-    
-  
-  requestAnimationFrame(render)
-}
 
-render()
+  tween.update(time)
+  requestAnimationFrame(render);
+};
+
+render();
